84aa9d04fea18f9e9f1939e1779549bbb53b67e7
Vinícius Queiroz <105626014+euovinq@users.noreply.github.com>
2023-09-18 15:24:16 -0300

    mudancas

--- PWMB_ENTREGAS/PWMB-Semana#06/functions.js
+++ PWMB_ENTREGAS/PWMB-Semana#06/functions.js
@@ -3,10 +3,15 @@ document.getElementById("formulario").addEventListener("submit", function (e) {
 
     var usuario = document.getElementById("usuario").value;
     var senha = document.getElementById("senha").value;
+    var usuario_logado = false;
     if(usuario == 'admin' && senha == 'admin'){
-        window.location.href = "index.html";
+        
+        usuario_logado = true;
+        window.location.href = "index.html?usuario_logado=" + usuario_logado;
+
     } else{
         alert("Usuario e senha incorretos !")
     }
     
 })
+
